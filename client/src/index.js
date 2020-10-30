8c656a932267798cb22a3604c24430a102520041
Austin Starks <as2779@cornell.edu>
2020-10-30 19:48:03 -0400

    Begin library

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import './index.css';
 import Home from "./components/Home/Home";
 import Example from "./components/APIExample";
-import './index.css';
 import App from './components/App';
+import Library from './components/Library/';
 
 render((
     <App>
@@ -12,6 +13,7 @@ render((
             <Switch>
                 <Route exact path='/' component={Home} />
                 <Route exact path='/example' component={Example} />
+                <Route exact path='/library' component={Library} />
             </Switch>
         </BrowserRouter>
     </App>
