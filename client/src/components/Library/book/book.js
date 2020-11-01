68495d4a424ea42556c41bc05af920f58a13d357
Austin Starks <as2779@cornell.edu>
2020-11-01 10:26:28 -0500

    Change to Bootstrap-modal

--- client/src/components/Library/book/book.js
+++ client/src/components/Library/book/book.js
@@ -1,27 +1,40 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import "./book.css" 
+import { Dropdown } from 'react-bootstrap'
+import $ from 'jquery';
+import "./book.css"
+
 
 class Book extends Component {
-    constructor(props) {
-        super(props);
-      }
+    state = { dropdownOpen: false }
 
 
+    triggerBookshelfPopup = () => {
+        console.log("click")
+        $('#bookshelfModal').modal('show');
+    }
+
+    toggle = () => {
+        const opened = this.state.dropdownOpen;
+        this.setState({ dropdownOpen: !opened });
+    }
 
     render() {
-     
-
         return (
-            <div className="book" role="button" data-toggle="dropdown"
-                              aria-haspopup="true" aria-expanded="false">
-             <div className="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                            {/* Add click handlet to Add to Bookshelf */}
-                            {/* Popup that displays all of the bookshelf */}
-                          <p className="dropdown-item status" id="ok-status">Add To Bookshelf</p>
-                          <p className="dropdown-item status" id="help-status">Recommend To Friend</p>
-                        </div>
+            <div>
+                <Dropdown >
+                    <div className="d-flex">
+                        <div className="book">
+                        </div>
+                        <Dropdown.Toggle split variant="" id="dropdown-split-basic" />
+
+                    </div>
+                    <Dropdown.Menu>
+                        <Dropdown.Item onClick={() => console.log("click")} id="ok-status">Add To Bookshelf</Dropdown.Item>
+                        <Dropdown.Item id="help-status">Recommend To Friend</Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
             </div>
+
 
         );
     }
