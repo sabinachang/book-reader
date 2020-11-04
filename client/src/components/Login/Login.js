154443e429cfa8a32c7e3225f729719a6a143f04
Mimi Shih <meitsens@andrew.cmu.edu>
2020-11-03 22:32:56 -0500

    reorganize login and register views

---
+++ client/src/components/Login/Login.js
@@ -0,0 +1,72 @@
+import React, {useState} from 'react';
+import axios from 'axios';
+import './Login.css';
+
+function Login (props) {
+    const [state , setState] = useState({
+        email : "",
+        password : "",
+        successMessage: null
+    })
+    const handleChange = (e) => {
+        const {id , value} = e.target   
+        setState(prevState => ({
+            ...prevState,
+            [id] : value
+        }))
+    }
+
+    const handleSubmitClick = (e) => {
+        redirectToHome();
+    }
+
+    const redirectToHome = () => {
+        props.history.push('/');
+    }
+    const redirectToRegister = () => { 
+        props.updateTitle('Register');
+    }
+    return(
+        <div className="card col-12 col-lg-4 login-card mt-2 hv-center">
+            <form>
+                <div className="form-group text-left">
+                <label htmlFor="exampleInputEmail1">Email address</label>
+                <input type="email" 
+                       className="form-control" 
+                       id="email" 
+                       aria-describedby="emailHelp" 
+                       placeholder="Enter email" 
+                       value={state.email}
+                       onChange={handleChange}
+                />
+                </div>
+                <div className="form-group text-left">
+                <label htmlFor="exampleInputPassword1">Password</label>
+                <input type="password" 
+                       className="form-control" 
+                       id="password" 
+                       placeholder="Password"
+                       value={state.password}
+                       onChange={handleChange} 
+                />
+                </div>
+                <div className="form-check">
+                </div>
+                <button 
+                    type="submit" 
+                    className="btn btn-primary"
+                    onClick={handleSubmitClick}
+                >Submit</button>
+            </form>
+            <div className="alert alert-success mt-2" style={{display: state.successMessage ? 'block' : 'none' }} role="alert">
+                {state.successMessage}
+            </div>
+            <div className="registerMessage">
+                <span>Dont have an account? </span>
+                <span className="loginText" onClick={() => redirectToRegister()}>Register</span> 
+            </div>
+        </div>
+    )
+}
+
+export default Login;
