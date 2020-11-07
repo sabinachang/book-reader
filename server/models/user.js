f35b1523810c9462e4bf24c146fa3f5961052a31
sabinachang <sabchang1@gmail.com>
2020-11-08 00:43:38 +0800

    Update user schema to include recommend shelf

--- server/models/user.js
+++ server/models/user.js
@@ -13,7 +13,16 @@ const userSchema = new mongoose.Schema({
     },
     hash: String,
     salt: String,
+    recommend: [{type: mongoose.Schema.Types.ObjectId, ref: 'Book'}],
 });
+
+userSchema.statics.displayRecommendShelf = async function displayRecommendShelf() {
+    const { recommend } = await this.findOne({ username: 'sab4'}, 'recommend -_id')
+                            .populate({ path: 'recommend'});
+
+    return await mongoose.model('Book').populate(recommend,
+        [{ path: 'flyweight'}, { path: 'owner', select: 'username'}]);
+}
 
 const User = mongoose.model('User', userSchema);
 
