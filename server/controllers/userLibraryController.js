9d09d11ac6ab4ca58a0df0eaa6bb1e49e25c2b36
Jiaxin Du <jiaxind@andrew.cmu.edu>
2020-11-12 05:38:18 +0900

    use singleton pattern for api

--- server/controllers/userLibraryController.js
+++ server/controllers/userLibraryController.js
@@ -1,12 +1,16 @@
-
+const getBookSearch = require('../models/bookSearch');
 const axios = require('axios');
+
+const bookSearch = getBookSearch()
 
 exports.getBookResult = async function getBookResult(req, res, next) {
 	if (req.params.query) {
 		query = req.params.query;
 		bookResult = null;
-		console.log(query);
-		await axios.get('https://www.googleapis.com/books/v1/volumes?q=' + query)
+		baseurl = 'https://www.googleapis.com/books/v1/volumes?q=' 
+		keyurl = ':keyes&key=' + bookSearch.getKey();
+		console.log('search query: ', query)
+		await axios.get(baseurl + query + keyurl)
 		.then((res) => {
 			bookResult = res.data.items;
 		})
