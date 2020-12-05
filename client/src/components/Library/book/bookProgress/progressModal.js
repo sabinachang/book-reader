e3ffba10c70ccce7eccc88770d176da076e80f6a
Austin Starks <as2779@cornell.edu>
2020-12-05 13:10:31 -0500

    Add book progress to chain of responsibility

--- client/src/components/Library/book/bookProgress/progressModal.js
+++ client/src/components/Library/book/bookProgress/progressModal.js
@@ -10,20 +10,21 @@ class ProgressModal extends React.Component {
             value: '',
             progress: '0',
             errMsg: '',
-            pgsMsg: ''};
+            pgsMsg: ''
+        };
         this.needUpdate = true;
     }
 
     handleChange = (e) => {
         if (e.target) {
-            this.setState({value: e.target.value});
+            this.setState({ value: e.target.value });
         }
-        
+
     }
 
     handleSubmit = (e) => {
-        e.preventDefault(); 
-        this.setState({errMsg: ''});
+        e.preventDefault();
+        this.setState({ errMsg: '' });
         if (this.state.value) {
             const pageNum = Number(this.state.value);
             if (this.checkValidInput(pageNum)) {
@@ -36,73 +37,74 @@ class ProgressModal extends React.Component {
         if (Number.isInteger(userInput)) {
             if (this.props.bookInfo.pageCount &&
                 userInput > this.props.bookInfo.pageCount) {
-                this.setState({errMsg: 'Please enter the page number < total page', value: '', pgsMsg: ''});
+                this.setState({ errMsg: 'Please enter the page number < total page', value: '', pgsMsg: '' });
                 return false;
             } else if (userInput <= 0) {
-                this.setState({errMsg: 'Please enter the page number > 0', value: '', pgsMsg: ''});
+                this.setState({ errMsg: 'Please enter the page number > 0', value: '', pgsMsg: '' });
                 return false;
             } else {
                 return true;
             }
         } else {
-            this.setState({errMsg: 'Please enter an integer page number', value: '', pgsMsg: ''});
+            this.setState({ errMsg: 'Please enter an integer page number', value: '', pgsMsg: '' });
             return false;
         }
     }
 
     closeModal = () => {
-        this.setState({value: '', progress: '0', errMsg: '', pgsMsg: ''});
+        this.setState({ value: '', progress: '0', errMsg: '', pgsMsg: '' });
         this.needUpdate = true;
         this.props.handleClose();
     }
 
     updateProgress = (inputPage) => {
         axios.put('/api/progress', {
+            request_type: "book-progress",
             isbn: this.props.bookInfo.isbn,
             pageNum: inputPage
-            },
-            {withCredentials: true}
-        ).then ((res) => {
+        },
+            { withCredentials: true }
+        ).then((res) => {
             if (res.status === 200) {
-                this.setState({progress: res.data.message, value: ''});
+                this.setState({ progress: res.data.message, value: '' });
                 this.checkProgress();
             } else {
                 console.log(res.error);
             }
-   
-        }).catch ((err) => {
+
+        }).catch((err) => {
             console.log(err);
         });
     }
 
     getProgress = () => {
         axios.get(`/api/progress/${this.props.bookInfo.isbn}`,
-            {withCredentials: true}
-        ).then ((res) => {
+            { withCredentials: true }
+        ).then((res) => {
             if (res.status === 200) {
                 // console.log(res.data.message);
-                this.setState({progress: res.data.message});
+                this.setState({ progress: res.data.message });
             } else {
                 console.log(res.error);
             }
-   
-        }).catch ((err) => {
+
+        }).catch((err) => {
             console.log(err);
         });
     }
 
     checkProgress = () => {
         if (this.state.progress === 100) {
-            this.setState({pgsMsg: 'Congratulations! This book will move to Read bookshelf!'});   
+            this.setState({ pgsMsg: 'Congratulations! This book will move to Read bookshelf!' });
         } else if (this.state.progress < 50) {
-            this.setState({pgsMsg: 'Hope you like this book!'});
+            this.setState({ pgsMsg: 'Hope you like this book!' });
         } else {
-            this.setState({pgsMsg: 'Almost done! Good job!'});
+            this.setState({ pgsMsg: 'Almost done! Good job!' });
         }
     }
 
     getCurrentProgress = () => {
-        if (this.needUpdate){
+        if (this.needUpdate) {
             this.getProgress();
             this.needUpdate = false;
         }
@@ -111,39 +113,39 @@ class ProgressModal extends React.Component {
 
     getProgressUI = () => {
         return (<div>
-                    <div>
-                        <h5>Current progress: {this.getCurrentProgress()}%</h5>
-                        <p>Total Page: {this.props.bookInfo.pageCount}</p>
-                    </div>
+            <div>
+                <h5>Current progress: {this.getCurrentProgress()}%</h5>
+                <p>Total Page: {this.props.bookInfo.pageCount}</p>
+            </div>
 
 
-                    <div className='input-container'>
-                        <form onSubmit={this.handleSubmit} className='input-form'>
-                            <label className='input-label'>Last page you read: 
+            <div className='input-container'>
+                <form onSubmit={this.handleSubmit} className='input-form'>
+                    <label className='input-label'>Last page you read:
                                 <input type='number' pattern='[0-9]*'
-                                 value={this.state.value} className='input-num'
-                                 onChange={this.handleChange}/>
-                            </label>
-                            <input type='submit' value='Update' className='btn-primary'/>
-                        </form>
-                    </div>
+                            value={this.state.value} className='input-num'
+                            onChange={this.handleChange} />
+                    </label>
+                    <input type='submit' value='Update' className='btn-primary' />
+                </form>
+            </div>
 
-                    <div className="alert alert-danger mt-2" style={{display: this.state.errMsg ? 'block' : 'none' }} role="alert">
-                        {this.state.errMsg}
-                    </div>
-                    <div className="alert alert-info mt-2" style={{display: this.state.pgsMsg ? 'block' : 'none' }} role="alert">
-                        {this.state.pgsMsg}
-                    </div>
-                </div>
-            )
+            <div className="alert alert-danger mt-2" style={{ display: this.state.errMsg ? 'block' : 'none' }} role="alert">
+                {this.state.errMsg}
+            </div>
+            <div className="alert alert-info mt-2" style={{ display: this.state.pgsMsg ? 'block' : 'none' }} role="alert">
+                {this.state.pgsMsg}
+            </div>
+        </div>
+        )
     }
 
     render() {
-            return (<Modal
+        return (<Modal
             visible={this.props.visible}
             handleClose={() => this.closeModal()}
             heading="Tracking Progress">
-            {this.getProgressUI()}    
+            {this.getProgressUI()}
         </Modal>)
     }
 }
