a0b0d017b2f3b28842425b2ce32e2f85fe44ce74
Austin Starks <as2779@cornell.edu>
2020-12-05 13:41:41 -0500

    Make controllers into objects

--- server/controllers/loginController.js
+++ server/controllers/loginController.js
@@ -2,53 +2,54 @@ const { User, findUserByUsername, updateOnlineStatus } = require('../models/user
 const { validatePassword } = require('../lib/password');
 const { createToken } = require('../lib/jwt');
 
-class LoginController {
-    static async login(req, res) {
-        const { username, password } = req.body;
-        
-        try {
-          const user = await findUserByUsername(username);    
-          // username does not exists
-          if (!user) {
-            res.status(400).json({
-                error: 'Username does not exist',
-            });
-          } else {
-            // user exists, check if password is correct
-           if (validatePassword(password, user.hash, user.salt)) {
-              // generate jwt and return it in response
-              const token = createToken(user);
-              const cookieMaxAge = 3 * 24 * 60 * 60 * 1000;
-              res.cookie('jwt', token, {
-                maxAge: cookieMaxAge,
-              });
-              res.cookie('username', username, {
-                maxAge: cookieMaxAge,
-              })
-              // User.updateOnlineStatus(username, true).then(() => {
-                res.status(200).send({ message: 'login successfully' });
-              // });
-            } else {
-              res.status(400).json({
-                error: 'Password incorrect',
-              });
-            }
-          }
-        } catch (err) {
-          res.status(400).json({ error: err.message });
-        }
-    }
+const login = async function (req, res) {
+  const { username, password } = req.body;
 
-    // static logout(req, res) {
-    //   const { username } = req.body;
-    //   User.updateOnlineStatus(username, false)
-    //     .then(() => {
-    //       res.status(200).end();
-    //     })
-    //     .catch((err) => {
-    //       res.status(400).json({ error: err.message });
-    //     });
-    // }
+  try {
+    const user = await findUserByUsername(username);
+    // username does not exists
+    if (!user) {
+      res.status(400).json({
+        error: 'Username does not exist',
+      });
+    } else {
+      // user exists, check if password is correct
+      if (validatePassword(password, user.hash, user.salt)) {
+        // generate jwt and return it in response
+        const token = createToken(user);
+        const cookieMaxAge = 3 * 24 * 60 * 60 * 1000;
+        res.cookie('jwt', token, {
+          maxAge: cookieMaxAge,
+        });
+        res.cookie('username', username, {
+          maxAge: cookieMaxAge,
+        })
+        // User.updateOnlineStatus(username, true).then(() => {
+        res.status(200).send({ message: 'login successfully' });
+        // });
+      } else {
+        res.status(400).json({
+          error: 'Password incorrect',
+        });
+      }
+    }
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
 }
 
-module.exports = LoginController;
+// static logout(req, res) {
+//   const { username } = req.body;
+//   User.updateOnlineStatus(username, false)
+//     .then(() => {
+//       res.status(200).end();
+//     })
+//     .catch((err) => {
+//       res.status(400).json({ error: err.message });
+//     });
+// }
+
+
+module.exports = {
+  login
+};
--- server/controllers/progressController.js
+++ server/controllers/progressController.js
@@ -1,80 +1,81 @@
-const Bookshelves = require('../models/bookshelves');
 const Book = require('../models/book');
 const User = require('../models/user').User;
 const BookFlyweight = require('../models/bookFlyweight');
 const observer = require('../lib/observer');
 
 
-class progressController {
-    static async updateProgress(req, res) {
-    	if (req && req.cookies && req.cookies.username && 
-            req.body && req.body.isbn && req.body.pageNum) {
-    		const username = req.cookies.username;
-    		const owner = await User.findOne({username: username});
-    		var flyweight = await BookFlyweight.get(req.body.isbn);
-    		var book = await Book.findOne({flyweight: flyweight, owner: owner});
-    		const totalPage = flyweight.pageCount;
+const updateProgress = async function (req, res) {
+    if (req && req.cookies && req.cookies.username &&
+        req.body && req.body.isbn && req.body.pageNum) {
+        const username = req.cookies.username;
+        const owner = await User.findOne({ username: username });
+        var flyweight = await BookFlyweight.get(req.body.isbn);
+        var book = await Book.findOne({ flyweight: flyweight, owner: owner });
+        const totalPage = flyweight.pageCount;
 
-            if (totalPage) { //book.progress>=0 && 
-                const progress = Book.calculateProgress(req.body.pageNum, totalPage);
-                console.log('progress: ',progress,'%');
-                try {
-                    await Book.updateProgress(flyweight, owner, progress);
-                    res.status(200).json({message: progress});
-                    await observer.progressObserver(progress, owner, book);
-                } catch (err) {
-                    console.log(err);
-                }     
-            } else {
-                res.status(400);
-                res.json({'error': 'No permission to track progress'});
+        if (totalPage) { //book.progress>=0 && 
+            const progress = Book.calculateProgress(req.body.pageNum, totalPage);
+            console.log('progress: ', progress, '%');
+            try {
+                await Book.updateProgress(flyweight, owner, progress);
+                res.status(200).json({ message: progress });
+                await observer.progressObserver(progress, owner, book);
+            } catch (err) {
+                console.log(err);
             }
-    	} else {
-
-    		res.status(400).json({'error': 'Missing params'})
-    	}
-    }
-
-    static async getProgress(req, res) {
-        try {
-            if (req && req.cookies && req.cookies.username 
-                && req.params && req.params.isbn) {
-                const username = req.cookies.username;
-                const owner = await User.findOne({username: username});
-                var flyweight = await BookFlyweight.get(req.params.isbn);
-                var book = await Book.findOne({flyweight: flyweight, owner: owner});
-                if (book.progress !== null && book.progress>=0) {
-                    res.status(200).json({message: book.progress})
-                } else {
-                    res.status(400).json({'error': 'Progress Undefined'})
-                }
-            } else {
-                res.status(400).json({'error': 'Missing params'})
-            }
-        } catch (err) {
-            console.log(err);
+        } else {
+            res.status(400);
+            res.json({ 'error': 'No permission to track progress' });
         }
+    } else {
+
+        res.status(400).json({ 'error': 'Missing params' })
     }
-
-    // get all reading progress
-    // static async getProgress(req, res) {
-    //     if (req && req.cookies && req.cookies.username && req.params && req.params.bookshelf) {
-    //         const username = req.cookies.username;
-    //         const owner = await User.findOne({username: username});
-    //         const reading_id = await Bookshelves.getBooks(owner, 'reading');
-
-    //         result = []
-    //         for (i=0; i<reading.length; i++) {
-    //             var reading = await Book.findById(reading_id[i]);
-    //             console.log(reading.progress)
-    //             result.push(reading.progress)
-    //         }
-    //         res.json({'result': result}).status(200);
-    //     } else {
-    //         res.status(400).json({'error': 'Missing params'})
-    //     }
-    // }
-
 }
 
-module.exports = progressController;
+const getProgress = async function (req, res) {
+    try {
+        if (req && req.cookies && req.cookies.username
+            && req.params && req.params.isbn) {
+            const username = req.cookies.username;
+            const owner = await User.findOne({ username: username });
+            var flyweight = await BookFlyweight.get(req.params.isbn);
+            var book = await Book.findOne({ flyweight: flyweight, owner: owner });
+            if (book.progress !== null && book.progress >= 0) {
+                res.status(200).json({ message: book.progress })
+            } else {
+                res.status(400).json({ 'error': 'Progress Undefined' })
+            }
+        } else {
+            res.status(400).json({ 'error': 'Missing params' })
+        }
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+// get all reading progress
+// static async getProgress(req, res) {
+//     if (req && req.cookies && req.cookies.username && req.params && req.params.bookshelf) {
+//         const username = req.cookies.username;
+//         const owner = await User.findOne({username: username});
+//         const reading_id = await Bookshelves.getBooks(owner, 'reading');
+
+//         result = []
+//         for (i=0; i<reading.length; i++) {
+//             var reading = await Book.findById(reading_id[i]);
+//             console.log(reading.progress)
+//             result.push(reading.progress)
+//         }
+//         res.json({'result': result}).status(200);
+//     } else {
+//         res.status(400).json({'error': 'Missing params'})
+//     }
+// }
+
+
+
+module.exports = {
+    updateProgress,
+    getProgress
+};
