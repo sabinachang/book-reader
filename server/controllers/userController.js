bd68d640f4cd23db71dd777009e0501ca4b0032b
Austin Starks <as2779@cornell.edu>
2020-12-05 16:08:28 -0500

    Combine controllers

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,41 +1,99 @@
 const { createNewUser, findUserByUsername } = require('../models/user');
-
+const { validatePassword } = require('../lib/password');
 const { genHashAndSalt } = require('../lib/password');
-const { createToken } = require('../lib/jwt');
+const { createToken, verifyToken } = require('../lib/jwt');
 
 
-class UserController {
-  static createUser(req, res) {
-    const { username, password } = req.body;
-    // create new user and save to db
-    const { hash, salt } = genHashAndSalt(password);
-    createNewUser(username, hash, salt)
-      .then(async () => {
-        console.log('create user successfully', username);
-        const user = await findUserByUsername(username);
+const createUser = function (req, res) {
+  const { username, password } = req.body;
+  // create new user and save to db
+  const { hash, salt } = genHashAndSalt(password);
+  createNewUser(username, hash, salt)
+    .then(async () => {
+      console.log('create user successfully', username);
+      const user = await findUserByUsername(username);
+      const token = createToken(user);
+      const cookieMaxAge = 3 * 24 * 60 * 60;
+      res.cookie('jwt', token, {
+        maxAge: cookieMaxAge * 1000,
+      });
+      res.cookie('username', username, {
+        maxAge: cookieMaxAge * 1000,
+      })
+      res.status(201).json({ message: 'create user successfully' });
+    })
+    .catch((err) => {
+      console.log(err)
+      let message;
+      if (err.code === 11000) {
+        console.log('Username already exists');
+        message = 'Username already exists';
+      } else {
+        message = err;
+      }
+      res.status(400).json({ error: message });
+    });
+}
+
+const login = async function (req, res) {
+  const { username, password } = req.body;
+  try {
+    const user = await findUserByUsername(username);
+    // username does not exists
+    if (!user) {
+      res.status(400).json({
+        error: 'Username does not exist',
+      });
+    } else {
+      // user exists, check if password is correct
+      if (validatePassword(password, user.hash, user.salt)) {
+        // generate jwt and return it in response
         const token = createToken(user);
-        const cookieMaxAge = 3 * 24 * 60 * 60;
+        const cookieMaxAge = 3 * 24 * 60 * 60 * 1000;
         res.cookie('jwt', token, {
-          maxAge: cookieMaxAge * 1000,
+          maxAge: cookieMaxAge,
         });
         res.cookie('username', username, {
-          maxAge: cookieMaxAge * 1000,
+          maxAge: cookieMaxAge,
         })
-        res.status(201).send({ message: 'create user successfully' });
-      })
-      .catch((err) => {
-        console.log(err)
-        let message;
-        if (err.code === 11000) {
-          console.log('Username already exists');
-          message = 'Username already exists';
-        } else {
-          message = err;
-        }
-        res.status(400).json({ error: message });
-      });
+        // User.updateOnlineStatus(username, true).then(() => {
+        res.status(200).send({ message: 'login successfully' });
+        // });
+      } else {
+        res.status(400).json({
+          error: 'Password incorrect',
+        });
+      }
+    }
+  } catch (err) {
+    res.status(400).json({ error: err.message });
   }
 }
 
-module.exports = UserController;
+const logout = function (req, res) {
+  res.clearCookie('jwt');
+  res.clearCookie('username')
+  res.status(200).json({ message: "logged out successfully" })
+}
+
+const checkAuth = function (req, res) {
+  try {
+    const payload = verifyToken(req.cookies.jwt)
+    if (!req.cookies.username || payload.username !== req.cookies.username) {
+      res.status(403).json({ message: "unauthenticated" })
+    } else {
+      res.status(200).json({ message: 'authenticated' })
+    }
+  } catch (err) {
+    res.status(403).json({ message: "unauthenticated" })
+  }
+
+}
+
+module.exports = {
+  createUser,
+  login,
+  logout,
+  checkAuth
+};
 
