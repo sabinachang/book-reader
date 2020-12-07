65a0b845d208374687f33a659cae2217893190f6
Jiaxin Du <jiaxind@andrew.cmu.edu>
2020-12-07 13:18:58 +0900

    add top favorites books backend

--- test/test-example.js
+++ test/test-example.js
@@ -3,13 +3,11 @@ const getBookSearch = require('../server/models/bookSearch');
 const bookSearch = getBookSearch();
 
 test('Test get google book search api', function() {
-	const baseUrl = bookSearch.getBaseUrl()
+	const baseUrl = bookSearch.getBaseUrl();
 	expect(baseUrl).toBe('https://www.googleapis.com/books/v1/volumes?q=');
-
 })
 
 test('Test get google book search index parameter', function() {
 	const indexparam = bookSearch.getStartIndex('2');
 	expect(indexparam).toBe('&startIndex=2');
-
 })
